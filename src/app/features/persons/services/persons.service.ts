f2180b2a961e70073b093b13b051e5e0df428e48
noellepaes <noellepaes12345@gmail.com>
2025-04-22 15:05:18 -0300

    connect back for people

--- src/app/features/persons/services/persons.service.ts
+++ src/app/features/persons/services/persons.service.ts
@@ -1,7 +1,7 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Person } from '../persons.module';
 
 
@@ -9,22 +9,26 @@ import { Person } from '../persons.module';
   providedIn: 'root'
 })
 export class PersonService {
-  // private apiUrl = 'http://localhost:8080/persons';
+  private apiUrl = 'http://localhost:8080/persons';
 
-  // constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  private personsMock: Person[] = [
-    { cpf: '123', name: 'Alice', fined: false, allowedBorrow: true },
-    { cpf: '456', name: 'Bob',   fined: true,  allowedBorrow: false },
-    { cpf: '789', name: 'Carol', fined: false, allowedBorrow: true }
-  ];
+  getPersons(): Observable<Person[]> {
+    return this.http.get<Person[]>(this.apiUrl);
+  }
 
-  // getPersons(): Observable<any[]> {
-  //   return this.http.get<any[]>(this.apiUrl);
-  // }
-   // Retorna o mock como Observable
-   getPersons(): Observable<Person[]> {
-    return of(this.personsMock);
+  addPerson(person: Person): Observable<Person> {
+    return this.http.post<Person>(this.apiUrl, person);
+  }
+
+  updatePerson(person: Person): Observable<Person> {
+    return this.http.put<Person>(
+      `${this.apiUrl}/${person.cpf}`, person
+    );
+  }
+
+  deletePerson(cpf: string): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${cpf}`);
   }
 }
 
