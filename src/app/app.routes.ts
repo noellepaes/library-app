86750361214fdb9625a56c79a0d39d94ed7e2027
noellepaes <noellepaes12345@gmail.com>
2025-04-22 10:08:05 -0300

    routing ok

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 // src/app/app.routes.ts
 import { Routes } from '@angular/router';
 import { PersonsListComponent } from './features/persons/components/persons-list/persons-list.component';
+import { HomeComponent } from './home/components/home.component';
+import { LoansListComponent } from './features/loans/loans-list/loans-list.component';
+import { BookListComponent } from './features/books/components/book-list/book-list.component';
 
 export const routes: Routes = [
+  { path: '', component: HomeComponent },
   { path: 'persons', component: PersonsListComponent },
-  { path: '', redirectTo: 'persons', pathMatch: 'full' },
+  { path: 'loans', component: LoansListComponent },
+  { path: 'books', component: BookListComponent },
   { path: '**', redirectTo: 'persons' }
 ];
 
